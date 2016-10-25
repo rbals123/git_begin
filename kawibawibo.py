92fa85f7b0d81b1c2b49e6b1b6dd49cef04bc95c
unknown <>
2016-10-25 23:49:50 +0900

    kawibawibo_1.py

--- kawibawibo.py
+++ kawibawibo.py
@@ -5,10 +5,10 @@ def main():
 	com_finger = random.randrange(3) + 1
 	for i in range(10):
 
-		my_finger = int(input("가위(0), 바위(1), 보(2)를 입력하세요. : "))
+		my_finger = int(input("가위(1), 바위(2), 보(3)를 입력하세요. : "))
 
 		while not(my_finger == 1 or my_finger == 2 or my_finger ==3):
-		    my_finger = int(input("가위(0), 바위(1), 보(2)를 입력하세요. : "))
+		    my_finger = int(input("가위(1), 바위(2), 보(3)를 입력하세요. : "))
 		if(com_finger == 0):
 			if(my_finger == 0):
 				print("컴퓨터는 가위를 냈습니다 => 비김~")
